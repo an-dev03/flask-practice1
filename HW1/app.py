ca35d0c9e4ed7cda7c7f32b6776f984e9d924b57
an-dev03 <taehyun3517@gmail.com>
2026-09-22 16:07:07 +0900

    greet page completed

--- HW1/app.py
+++ HW1/app.py
@@ -10,5 +10,9 @@ def home_page():
 def profile_page():
     return render_template("profile.html", hobbies=["운동", "게임", "독서"])
 
+@app.route("/greet/<name>")
+def greet_page(name):
+    return render_template("greet.html", name=name)
+
 if __name__ == "__main__":
     app.run(debug=True)
